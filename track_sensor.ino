7a9e5092d6ea9a20e84f638da928616f7d7e4680
Johan Casier <johan@jcasier.com>
2021-10-18 15:07:43 -0700

    created

---
+++ track_sensor.ino
@@ -0,0 +1,116 @@
+
+// CT Sensor test
+// Using UNO built-in ADC to read the sensor
+//////////////////////////////////////////////////////////////////
+
+#define VERSION "1.006"
+#define SYS_ID "CT Sensor Test - Direct to UNO ADC"
+const int adcpin = 0;
+
+// Sampling Parameters
+const unsigned long sampleTime = 2000UL; 
+const unsigned long numSamples = 100UL; 
+const unsigned long sampleInterval = sampleTime/numSamples; 
+
+#define SENSITIVITY 5000
+#define DETECTION_MULTIPLIER 1.3
+#define CALIBRATION_READS 300
+
+// variables to hold sensor quiescent readings
+float aqv;  // Average Quiescent Voltage; e.g. ADC Zero
+float aqc;  // Average Quiescent Current; 
+
+void setup()
+{
+  Serial.begin(9600);
+  //Serial.println(String(F(SYS_ID)) + String(F(" - SW:")) + String(F(VERSION)));
+  Serial.print("\nCalibrating the sensor at pin ");
+  Serial.println(adcpin);
+  aqv = determineVQ(adcpin); 
+  Serial.print("AQV: ");
+  Serial.print(aqv * 1000, 4);
+  Serial.print(" mV\t");
+  aqc = determineCQ(adcpin, aqv);
+  Serial.print("AQC: ");
+  Serial.print(aqc * 1000, 4);
+  Serial.print(" mA\t");
+  float sense = (aqc * DETECTION_MULTIPLIER) - aqc;
+  Serial.print("Detection Sensitivity: ");
+  Serial.print(sense * 1000, 3);
+  Serial.println(" mA\n\n");
+  delay(7500);
+  pinMode(22, OUTPUT);
+  digitalWrite(22, LOW);
+}
+
+void loop(){
+  float current = readCurrent(adcpin, aqv);
+  float delta = abs(aqc - current);
+  bool occupied = delta > ((aqc * DETECTION_MULTIPLIER) - aqc);
+
+  Serial.print("Current Sensed: ");
+  Serial.print(current * 1000,3);
+  Serial.print(" mA\t");
+
+  if(occupied){
+    Serial.println("Occupied");
+    digitalWrite(22, HIGH);
+  } 
+  else {
+    Serial.println("Not occupied");
+    digitalWrite(22, LOW);
+  }
+  delay(3000);
+}
+
+//////////////////////////////////////////
+// Current Sensor Functions
+//////////////////////////////////////////
+float readCurrent(int pin, float adc_zero)
+{
+  float currentAcc = 0;
+  unsigned int count = 0;
+  unsigned long prevMicros = micros() - sampleInterval ;
+  while (count < numSamples)
+  {
+    if (micros() - prevMicros >= sampleInterval)
+    {
+      float adc_raw = (float) analogRead(pin) - adc_zero; // sensor reading in volts
+      adc_raw /= SENSITIVITY; // convert to amperes
+      currentAcc += (adc_raw * adc_raw); // sum the squares
+      count++;
+      prevMicros += sampleInterval;
+    }
+  }
+  //https://en.wikipedia.org/wiki/Root_mean_square
+  float rms = sqrt((float)currentAcc / (float)numSamples);
+  return rms;
+}
+
+//////////////////////////////////////////
+// Calibration
+// Track Power must be OFF during calibration
+//////////////////////////////////////////
+
+float determineVQ(int pin) {
+  float VQ = 0;
+  //read a large number of samples to stabilize value
+  for (int i = 0; i < CALIBRATION_READS; i++) {
+    VQ += analogRead(pin);
+    delayMicroseconds(sampleInterval);
+  }
+  VQ /= CALIBRATION_READS;
+  return VQ;
+}
+
+float determineCQ(int pin, float aqv) {
+  float CQ = 0;
+  // set reps so the total actual analog reads == CALIBRATION_READS
+  int reps = (CALIBRATION_READS / numSamples);
+  for (int i = 0; i < reps; i++) {
+    CQ += readCurrent(pin, aqv);
+  }
+  CQ /= reps;
+  return CQ;
+}
+
